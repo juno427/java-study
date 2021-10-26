96bb9997133ab7bdf8ee4bf6b0afe30d3e3e1f3c
JW <tkrkwl333@gmail.com>
2021-10-26 16:09:35 +0900

    2021-10-26

---
+++ CH19_Exeception/src/try_catch_finally/App.java
@@ -0,0 +1,10 @@
+package try_catch_finally;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CH20_GameOfLife/src/application/App.java
@@ -0,0 +1,22 @@
+package application;
+
+import javax.swing.SwingUtilities;
+
+import gui.MainFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 프로그램 시작
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new MainFrame();
+
+			}
+		});
+
+	}
+
+}
